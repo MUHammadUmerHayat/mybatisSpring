8a9178241a4bb5817c34f6b6266b81dc169d8a8c
Huanle Hu <huanlehu@Huanles-MacBook-Pro.local>
2018-05-13 00:55:17 +0900

    update comment

--- src/main/java/com/huanle/dao/UserMapper.java
+++ src/main/java/com/huanle/dao/UserMapper.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.huanle.model.Order;
 import com.huanle.model.User;
-
+//这个接口描述了mybatis的xml文件里的sql语句中方法的参数和返回值的类型
 public interface UserMapper {
 	public User getUserById(int id);
 	public List<Order> getUserOrders(int id);
